edd03b677e0a5464f08bfd730b5ff5b7facb805a
zhaojinbing <beyondjinbing@hotmail.com>
2020-11-23 18:46:47 +0800

    添加管道

--- bookspider/pipelines.py
+++ bookspider/pipelines.py
@@ -6,8 +6,24 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import ujson
 
 
 class BookspiderPipeline:
     def process_item(self, item, spider):
+        return item
+
+
+
+class JsonWriterPipeline(object):
+    def __init__(self):
+        self.file = open('items.jl', 'w')
+
+    def process_item(self, item, spider):
+        line = ujson.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(line)
+        print()
+        print('-' * 10)
+        print(item)
+        print('*' * 10)
         return item
